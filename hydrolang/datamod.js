56246456d614de220286d35e39a23ab5dde981f1
erazocar <erazcar8@aquacloud.net>
2021-09-28 19:06:27 -0500

    added hydrolang web component wrapper

--- hydrolang/datamod.js
+++ hydrolang/datamod.js
@@ -16,6 +16,40 @@ template.innerHTML =
 <slot></slot>
 `;
 
+//Template attached to the module
+const template2 = document.createElement('template');
+template.id = 'HYDROLANG-ML'
+template.innerHTML =
+    `
+<style>
+</style>
+<div><slot name=data-mod></slot></div>
+<slot></slot>
+`;
+
+class hydrolangml extends HTMLElement {
+    constructor() {
+        super()
+        let shadow = this.attachShadow({
+            mode: 'open'
+        })
+        shadow.append(template2.content.cloneNode(true))
+
+        //The events of slots changes are dealth with here. Create 
+        //object parameters and append them to the global dictionaries
+        this.shadowRoot.addEventListener("slotchange", (ev) => {
+            if (ev.target.appendChild == "") {
+                [...ev.target.assignedElements()]
+                .filter(el => el.nodeName == 'data-mod')
+                    .map(el => el.slot = 'data-mod')
+            } else {
+                console.log(`SLOT: ${ev.target.name} got`, ev.target.assignedElements())
+            }
+        })
+    }
+}
+
+
 //Web component for handling data module.
 class datamod extends HTMLElement {
     static get properties() {
@@ -71,9 +105,26 @@ class datamod extends HTMLElement {
         for (var i = 0; i < attr.length; i++) {
             var prop = attr[i]
             props[prop] = this.getAttribute(attr[i])
-            console.log(props[prop])
         }
         return props
+    }
+
+    grabKeyList(obj) {
+        return new Promise(resolve => {
+        setTimeout(() => {
+            var x = Object.keys(obj)
+            resolve(x)
+        }, 1)
+    })
+    }
+
+
+    
+    //this behavior can be changed depending on the type of data
+    handlewaterdata(data) {
+        var x = data
+        console.log(data);
+        return x
     }
 
     //main class to handle the inputs from the user.
@@ -83,7 +134,6 @@ class datamod extends HTMLElement {
             mode: 'open'
         })
         shadow.append(template.content.cloneNode(true))
-        let web = document.querySelector('data-mod')
 
         //The events of slots changes are dealth with here. Create 
         //object parameters and append them to the global dictionaries
@@ -93,17 +143,16 @@ class datamod extends HTMLElement {
                 .filter(el => el.nodeName == 'func-parameter')
                     .map(el => el.slot = 'func-parameters')
             } else {
-                var datamodprop = this.makePropertiesFromAttributes('data-mod')
-                console.log(datamodprop)
                 var r = ev.target.assignedElements()
-                console.log(r)
+                var datamodprop = this.makePropertiesFromAttributes('data-mod')
                 var ar = this.makePropertiesFromParameters(r)
-
+                var newdb = {}
                 for (var i = 0; i < ar.length; i++) {
-                    window.db[i] = {
+                    newdb[i] = {
                         [r[i].localName]: ar[i]
                     }
                 }
+                window.db[datamodprop.id] = newdb
                 console.log(`SLOT: ${ev.target.name} got`, ev.target.assignedElements())
             }
         })
@@ -111,13 +160,8 @@ class datamod extends HTMLElement {
 
     //asynchronous callback to call the data module and potentially the map module.
     async connectedCallback() {
-
-        //this behavior can be changed depending on the type of data
-        function handlewaterdata(data) {
-            var x = data
-            console.log(data);
-            return x
-        }
+        var keyvalues = await this.grabKeyList(window.db)
+        console.log(keyvalues)
 
         //Need to create indexes for each of the parameters to append them to the dictionaries
         function callDatabase() {
@@ -125,18 +169,17 @@ class datamod extends HTMLElement {
                 setTimeout(() => {
                     //This will be modified to be doing the appending iteratively
                     var ob = {
-                        ...window.db[0]
+                        ...window.db
                     }
-                    var ne = Object.assign(ob.parameters, db[1])
-                    var y = Object.assign(ne, db[2])
-                    resolve(y)
+                    resolve(ob)
                 }, 50);
             })
         }
 
         var asa = await callDatabase()
-        var results = await window.hydro.data.retrieve(asa, handlewaterdata)
+        console.log(asa)
     }
+    // var results = await window.hydro.data.retrieve(asa, this.handlewaterdata)
 }
 
 //class for handling parameters
@@ -155,3 +198,4 @@ class parameters extends HTMLElement {
 //Defining the web components into the DOM
 window.customElements.define('data-mod', datamod)
 window.customElements.define('func-parameter', parameters)
+window.customElements.define('hydrolang-ml', hydrolangml)
